d392ef06fc9bc15a740988537ffdfda0e239841c
yoshuawuyts <i@yoshuawuyts.com>
2014-12-26 22:57:41 +0100

    index: fix

--- index.js
+++ index.js
@@ -1,28 +1,20 @@
-/**
- * Module dependencies.
- */
+
 var wreq   = require('watchify-request')
 var assert = require('assert')
 
-/**
- * Expose `kw`.
- */
 module.exports = kw
 
-/**
- * Wrap `watchify-request` in koa middleware.
- *
- * @param {Function} bundler
- * @return {GeneratorFunction}
- * @api public
- */
+// Wrap `watchify-request` in koa middleware.
+// @param  {Function} bundler
+// @return {GeneratorFunction}
+// @api public
 function kw(bundler) {
   assert(typeof bundler, 'function')
   var handler = wreq(bundler)
   return function *watchifyRequest(next) {
     handler(this.req, this.res, function(err, body) {
       if (err) this.throw(err)
-      next()
+      this.response.body = body
     }.bind(this))
   }
 }
