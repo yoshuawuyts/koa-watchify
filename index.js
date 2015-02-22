c0898316334e0847aa2d8ab54f70930b58325ff8
Yoshua Wuyts <i@yoshuawuyts.com>
2015-02-22 17:18:17 +0100

    index: use promises

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 const wreq = require('watchify-request')
+const thenify = require('thenify')
 const assert = require('assert')
 
 module.exports = kw
 
 // Wrap `watchify-request` in koa middleware.
-// @param {Function} bundler
+// @param {Object} bundler
 // @return {Function*}
-// @api public
 function kw(bundler) {
-  assert.equal(typeof bundler, 'function')
-  var handler = wreq(bundler)
+  assert.equal(typeof bundler, 'object')
+  const handler = thenify(wreq(bundler))
+
+  // Koa middleware.
+  // @param {Function} next
+  // @return {Promise}
   return function *watchifyRequest(next) {
-    const res = yield handler.bind(handler, this.req, this.res)
-    if (res[0]) this.throw(err)
-    this.response.body = res[1]
-    yield next
+    const ctx = this
+
+    return yield handler(this.req, this.res)
+      .then(success)
+      .catch(failure)
+
+    function success(res) {
+      ctx.response.body = res
+    }
+
+    function failure(err) {
+      ctx.throw(err)
+    }
   }
 }
